9fd4c9b303b3269e72ea82425147ddea218b7268
Rokas Gudavicius <roxguda@gmail.com>
2017-09-26 00:21:14 +0100

    added Findlay's magic code(tm)

--- scraper.py
+++ scraper.py
@@ -68,7 +68,8 @@ uniquewords = list(set(words))
 
 
 for word in uniquewords:
-    syllables = len(re.findall("[aiouy]+e*|e(?!d$|ly).|[td]ed|le$", word))
+    #syllables = len(re.findall("[aiouy]+e*|e(?!d$|ly).|[td]ed|le$", word))
+    syllables = syllables(word)
     if syllables not in buildingblocks:
         buildingblocks.update({syllables:[word]})
     else:
@@ -116,7 +117,29 @@ print line1
 print line2
 print line3
     
-
+# Findlay's script
+def syllablecount(word):
+	syl = 0
+	vowels = 'aeiouy' #I know that y technically isn't a vowel
+	words = word.lower().strip('.:?!').split(" ")
+	for i in range(0, len(words)):
+		tmp = 0
+		word = words[i]
+		if word[0] in vowels:
+			tmp += 1
+		for i in range(1, len(word)):
+			if ((word[i] == 'a'or word[i] == 'o') and word[i-1] == 'i') or (word[i] in vowels and word[i-1] not in vowels) or (word[i] == 'i' and (word[i-1] == 'u')):
+				tmp += 1
+		if word[len(word) - 1] == 'e' and not word[len(word) - 2] == 'l':
+			tmp -= 1
+		if word[len(word) -2] == 'e' and (word[len(word)-1] == 'd' or word[len(word)-1] == 's'):
+			tmp -=1
+		if word[len(word)-2] == 's' and word[len(word)-1] == 'm':
+			tmp += 1
+		if tmp == 0:
+			tmp += 1
+		syl += tmp
+	return syl
 
 
 
